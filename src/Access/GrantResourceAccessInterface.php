803c9b15fef5ff7cef44f03572f1aea903d0d21e
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2022-07-21 16:34:03 +0300

    remove access

--- src/Access/GrantResourceAccessInterface.php
+++ src/Access/GrantResourceAccessInterface.php
@@ -11,4 +11,8 @@ interface GrantResourceAccessInterface extends ResourceOperationInterface
     public function grantAccessToResource(string $userId, string $resourceId, string $userForAccessId, string $accessType = 'owner',): bool;
 
     public function grantAccessToResourceType(string $userId, string $userForAccessId, string $accessType = 'owner',): bool;
+
+    public function removeAccessToResource(string $userId, string $resourceId, string $userForAccessId, string $accessType = 'owner',): bool;
+
+    public function removeAccessToResourceType(string $userId, string $userForAccessId, string $accessType = 'owner',): bool;
 }
